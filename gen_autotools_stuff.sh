b4dc92b2d5fa1d8ea842c74d7551aba427fcd86d
Gregory M. Turner <gmt@be-evil.net>
2014-01-30 21:44:15 -0800

    clarify gen scriptname and add sanity check
    Signed-off-by: Gregory M. Turner <gmt@be-evil.net>

---
+++ gen_autotools_stuff.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+[[ -f install.wrapper.c &&  -x gen_autotools_stuff.sh ]] || \
+	{ echo "Be in the source dir." >&2; exit 1; }
+
+rm -rvf autom4te.cache/ Makefile.in compile configure depcomp install-sh missing aclocal.m4
+
+autoreconf --install
+
